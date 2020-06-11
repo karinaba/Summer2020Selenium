ab43a9b6efbf75828d1deaedcd84485483defd1c
karinaqapro@gmail.com <karinaqapro@gmail.com>
2020-06-10 21:30:52 -0400

    day2 package commit

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -4,9 +4,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         driver.get("http://google.com");
+        Thread.sleep(3000);
+        driver.close();
     }
 }
